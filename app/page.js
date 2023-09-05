28bb9818ee9b4e8c4cbe3756298f221275457b41
Roshan <ItsRoshankSingh@gmail.com>
2023-09-05 14:43:20 -0500

    common layout for all page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,19 @@
+import Feed from "@components/Feed";
+
 
 export default function Home() {
   return (
-    <div><h1>Home Page</h1></div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & share
+        {/* max-md:hidden will hide the br in large devices */}
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">It's an AI prompting tool for modern world to discover,
+        create and share creative prompts
+      </p>
+      <Feed />
+    </section>
   )
 }
